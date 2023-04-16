0521546dd02cc742ae8f8e4267a1dd2fdec9c056
Annaneilan <90556160+Annaneilan@users.noreply.github.com>
2023-04-16 21:03:50 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,23 +1,18 @@
-import argparse
-import telegram
+import telebot
 import time
 
 def main():
-    # Отримати аргументи командного рядка
     parser = argparse.ArgumentParser()
     parser.add_argument('--bot-token', required=True, help='Telegram bot token')
     parser.add_argument('--chat-id', required=True, help='Telegram chat ID')
     args = parser.parse_args()
 
-    # Створити об'єкт бота
-    bot = telegram.Bot(token=args.bot_token)
+    bot = telebot.TeleBot(args.bot_token)
 
-    # Відправити повідомлення
     chat_id = args.chat_id
     message = 'Привіт, я працюю'
-    bot.send_message(chat_id=chat_id, text=message)
+    bot.send_message(chat_id, message)
 
-    # Зачекати 5 секунд
     time.sleep(5)
 
 if __name__ == '__main__':
